713c2da7ed8a840d3db0c552eb43ecf873ca1016
Louis Gadza <siuolgadza@gmail.com>
2023-04-02 10:55:51 +0200

    start theamakro

---
+++ theamakro_fe/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ theamakro_fe/src/components/NavigationBar.jsx
@@ -0,0 +1,89 @@
+import { Container, Form, Nav, Navbar, Row } from "react-bootstrap";
+import "font-awesome/css/font-awesome.min.css";
+import {
+  faHouse,
+  faSearch,
+  faUser,
+  faEnvelope,
+  faCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import "../style/Navigation.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const NavigationBar = () => {
+  return (
+    <Navbar>
+      <SearchBar />
+      <Container className="nav-container">
+        <Row>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav>
+            <ul className="d-flex align-items-center my-0 py-2 justify-content-between w-100">
+              <li className="d-flex align-items-center justify-content-center flex-column">
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faHouse}
+                    style={{ color: "white", fontSize: "20px" }}
+                  />
+                  <small>Home</small>
+                </Nav.Link>
+              </li>
+              <li className="d-flex align-items-center justify-content-center flex-column">
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faSearch}
+                    style={{ color: "white", fontSize: "20px" }}
+                  />
+                  <small>Search</small>
+                </Nav.Link>
+              </li>
+              <li className="d-flex align-items-center justify-content-center flex-column">
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    style={{ color: "white", fontSize: "20px" }}
+                  />
+                  <small>Messages</small>
+                </Nav.Link>
+              </li>
+              <li className="d-flex align-items-center justify-content-center flex-column">
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faCirclePlus}
+                    style={{ color: "white", fontSize: "20px" }}
+                  />
+                  <small>Sell</small>
+                </Nav.Link>
+              </li>
+              <li className="d-flex align-items-center justify-content-center flex-column">
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faUser}
+                    style={{ color: "white", fontSize: "20px" }}
+                  />
+                  <small>Profile</small>
+                </Nav.Link>
+              </li>
+            </ul>
+          </Nav>
+        </Row>
+      </Container>
+    </Navbar>
+  );
+};
+export default NavigationBar;
+
+const SearchBar = () => {
+  return (
+    <Container>
+      <Form className="d-flex w-100" onSubmit={(e) => e.preventDefault()}>
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+      </Form>
+    </Container>
+  );
+};
+// export default SearchBar
