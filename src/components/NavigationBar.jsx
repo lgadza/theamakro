1b1cfe26a9b41e9865b13a9cf3cb0009a8b2d2ae
Louis Gadza <siuolgadza@gmail.com>
2023-04-14 13:12:43 +0200

    Nav

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,85 +1,218 @@
-import { Container, Nav, Navbar, Row } from "react-bootstrap";
-import "font-awesome/css/font-awesome.min.css";
-import { Link } from "react-router-dom";
-import {
-  faHouse,
-  faSearch,
-  faUser,
-  faEnvelope,
-  faCirclePlus,
-} from "@fortawesome/free-solid-svg-icons";
+// import { Container, Nav, Navbar, Row } from "react-bootstrap";
+// import "font-awesome/css/font-awesome.min.css";
+// import { Link } from "react-router-dom";
+// import {
+//   faHouse,
+//   faSearch,
+//   faUser,
+//   faEnvelope,
+//   faCirclePlus,
+// } from "@fortawesome/free-solid-svg-icons";
+// import "../style/Navigation.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// const NavigationBar = () => {
+//   return (
+//     <Navbar>
+//       <Container className="nav-container text-center mx-0 d-flex align-items-center justify-content-center px-0">
+//         <Row className="w-100">
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Nav className="p-0">
+//             <ul className="d-flex w-100 align-items-center my-0 py-2 justify-content-between px-4">
+//               <li className="d-flex align-items-center justify-content-center flex-column">
+//                 <Link
+//                   to="/"
+//                   className="d-flex align-items-center justify-content-center flex-column"
+//                 >
+//                   <FontAwesomeIcon
+//                     icon={faHouse}
+//                     style={{ color: "white", fontSize: "20px" }}
+//                   />
+//                   <small>Home</small>
+//                 </Link>
+//               </li>
+//               <li className="d-flex align-items-center justify-content-center flex-column">
+//                 <Link
+//                   to="category"
+//                   className="d-flex align-items-center justify-content-center flex-column"
+//                 >
+//                   <FontAwesomeIcon
+//                     icon={faSearch}
+//                     style={{ color: "white", fontSize: "20px" }}
+//                   />
+//                   <small>Search</small>
+//                 </Link>
+//               </li>
+//               <li className="d-flex align-items-center justify-content-center flex-column">
+//                 <Link className="d-flex align-items-center justify-content-center flex-column">
+//                   <FontAwesomeIcon
+//                     icon={faEnvelope}
+//                     style={{ color: "white", fontSize: "20px" }}
+//                   />
+//                   <small>Messages</small>
+//                 </Link>
+//               </li>
+//               <li className="d-flex align-items-center justify-content-center flex-column">
+//                 <Link
+//                   to="/sell"
+//                   className="d-flex align-items-center justify-content-center flex-column"
+//                 >
+//                   <FontAwesomeIcon
+//                     icon={faCirclePlus}
+//                     style={{ color: "white", fontSize: "20px" }}
+//                   />
+//                   <small>Sell</small>
+//                 </Link>
+//               </li>
+//               <li className="d-flex align-items-center justify-content-center flex-column">
+//                 <Link
+//                   to={"profile"}
+//                   className="d-flex align-items-center justify-content-center flex-column"
+//                 >
+//                   <FontAwesomeIcon
+//                     icon={faUser}
+//                     style={{ color: "white", fontSize: "20px" }}
+//                   />
+//                   <small>Profile</small>
+//                 </Link>
+//               </li>
+//             </ul>
+//           </Nav>
+//         </Row>
+//       </Container>
+//     </Navbar>
+//   );
+// };
+// export default NavigationBar;
+import React from "react";
 import "../style/Navigation.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const NavigationBar = () => {
+import logo from "../../src/img/TM logo.png"
+
+
+const Menu = (props) => {
   return (
-    <Navbar>
-      <Container className="nav-container text-center mx-0 d-flex align-items-center justify-content-center px-0">
-        <Row className="w-100">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className="p-0">
-            <ul className="d-flex w-100 align-items-center my-0 py-2 justify-content-between px-4">
-              <li className="d-flex align-items-center justify-content-center flex-column">
-                <Link
-                  to="/"
-                  className="d-flex align-items-center justify-content-center flex-column"
-                >
-                  <FontAwesomeIcon
-                    icon={faHouse}
-                    style={{ color: "white", fontSize: "20px" }}
-                  />
-                  <small>Home</small>
-                </Link>
-              </li>
-              <li className="d-flex align-items-center justify-content-center flex-column">
-                <Link
-                  to="category"
-                  className="d-flex align-items-center justify-content-center flex-column"
-                >
-                  <FontAwesomeIcon
-                    icon={faSearch}
-                    style={{ color: "white", fontSize: "20px" }}
-                  />
-                  <small>Search</small>
-                </Link>
-              </li>
-              <li className="d-flex align-items-center justify-content-center flex-column">
-                <Link className="d-flex align-items-center justify-content-center flex-column">
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    style={{ color: "white", fontSize: "20px" }}
-                  />
-                  <small>Messages</small>
-                </Link>
-              </li>
-              <li className="d-flex align-items-center justify-content-center flex-column">
-                <Link
-                  to="/sell"
-                  className="d-flex align-items-center justify-content-center flex-column"
-                >
-                  <FontAwesomeIcon
-                    icon={faCirclePlus}
-                    style={{ color: "white", fontSize: "20px" }}
-                  />
-                  <small>Sell</small>
-                </Link>
-              </li>
-              <li className="d-flex align-items-center justify-content-center flex-column">
-                <Link
-                  to={"profile"}
-                  className="d-flex align-items-center justify-content-center flex-column"
-                >
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    style={{ color: "white", fontSize: "20px" }}
-                  />
-                  <small>Profile</small>
-                </Link>
-              </li>
-            </ul>
-          </Nav>
-        </Row>
-      </Container>
-    </Navbar>
+    <div className={`menu-container ${props.showMenu}`}>
+      <div className="overlay" />
+      <div className="menu-items">
+        <ul>
+          <li>
+            <a href="#welcome-section" onClick={props.toggleMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={props.toggleMenu}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects" onClick={props.toggleMenu}>
+              My work
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={props.toggleMenu}>
+              Contact Me
+            </a>
+          </li>
+        </ul>
+        {/* <SocialLinks /> */}
+      </div>
+    </div>
   );
 };
+
+/***********************
+  Nav Component
+ ***********************/
+
+const Nav = (props) => {
+  return (
+    <React.Fragment>
+      <nav id="navbar">
+        <div className="nav-wrapper">
+          <img className="brand"  src={logo} alt="logo" />
+          <a
+            onClick={props.toggleMenu}
+            className={
+              props.showMenu === "active" ? "menu-button active" : "menu-button"
+            }
+          >
+            <span />
+          </a>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+class NavigationBar extends React.Component {
+  state = {
+    menuState: false,
+  };
+
+  toggleMenu = () => {
+    this.setState((state) => ({
+      menuState: !state.menuState
+        ? "active"
+        : state.menuState === "deactive"
+        ? "active"
+        : "deactive",
+    }));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Menu toggleMenu={this.toggleMenu} showMenu={this.state.menuState} />
+        <Nav toggleMenu={this.toggleMenu} showMenu={this.state.menuState} />
+        {/* <Header />
+        <About />
+        <Projects />
+        <Contact /> */}
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount() {
+    const navbar = document.querySelector("#navbar");
+    const header = document.querySelector("#welcome-section");
+    // const forest = document.querySelector(".forest");
+    // const silhouette = document.querySelector(".silhouette");
+    // let forestInitPos = -300;
+
+    window.onscroll = () => {
+      let scrollPos =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      // if (scrollPos <= window.innerHeight) {
+      //   silhouette.style.bottom = `${parseInt(scrollPos / 6)}px`;
+      //   forest.style.bottom = `${parseInt(forestInitPos + scrollPos / 6)}px`;
+      // }
+
+      // if (scrollPos - 100 <= window.innerHeight)
+      //   header.style.visibility =
+      //     header.style.visibility === "hidden" && "visible";
+      // else header.style.visibility = "hidden";
+
+      if (scrollPos + 100 >= window.innerHeight)
+        navbar.classList.add("bg-active");
+      else navbar.classList.remove("bg-active");
+    };
+
+    (function navSmoothScrolling() {
+      const internalLinks = document.querySelectorAll('a[href^="#"]');
+      for (let i in internalLinks) {
+        if (internalLinks.hasOwnProperty(i)) {
+          internalLinks[i].addEventListener("click", (e) => {
+            e.preventDefault();
+            document.querySelector(internalLinks[i].hash).scrollIntoView({
+              block: "start",
+              behavior: "smooth",
+            });
+          });
+        }
+      }
+    })();
+  }
+}
 export default NavigationBar;
---
+++ src/pages/Sign/SignIn.jsx
@@ -0,0 +1,82 @@
+import { Col, Container, Row } from "react-bootstrap";
+import ProductCard from "../../components/ProductCard";
+import "../../style/Home.css";
+import Button1 from "../../components/Button";
+import Avatar from "../../components/Avatar";
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  return (
+    <Container className="mt-5 signIn">
+      <Row className="mb-2">
+        <small className="text-light pb-3">
+          Sign in so we can tailor make your TheaMakro experience
+        </small>
+        <div className="d-flex justify-content-around">
+          <Button1 btn_id="Register" />
+          <Button1 btn_id="Sign in" />
+        </div>
+      </Row>
+      <Row>
+        <div className="slide-content d-flex my-2">
+          {[...Array(10)].map((brand, index) => {
+            return (
+              <div className="d-flex flex-column align-items-center justify-content-center py-2 mx-2">
+                <Avatar
+                  width={80}
+                  height={80}
+                  src={
+                    "https://img.freepik.com/darmowe-ikony/awatara_318-158392.jpg"
+                  }
+                  alt={"avatar"}
+                  className="p-3 brand-container"
+                />
+                <div className="ml-2">
+                  <small>Steve</small>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Row>
+      {[...Array(4)].map((brands, index) => {
+        return (
+          <Row>
+            <div className="d-flex justify-content-between">
+              <div>
+                {" "}
+                <h6 className="mb-0 text-start">CROPP</h6>
+                <small>Brands you might like</small>
+              </div>
+              <small className="text-success align-item-end">See all</small>
+            </div>
+            <div className="slide-content d-flex align-items-center my-2">
+              {[...Array(10)].map((brand, index) => {
+                return (
+                  <div className="d-flex flex-column align-items-center justify-content-center py-2 mx-2">
+                    <ProductCard width={"10rem"} height={"10rem"} />
+                  </div>
+                );
+              })}
+              <div role="button" className="text-nowrap text-success">
+                See all
+              </div>
+            </div>
+          </Row>
+        );
+      })}
+      <Row>
+        {[...Array(20)].map((item, index) => {
+          return (
+            <Col className="my-2" xs={6} sm={6}>
+              <Link to="item_preview">
+                <ProductCard width="100%" height="10rem" />
+              </Link>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
+export default SignIn;
